4d3223c455d80d5240fa17970bfdb3d5c97490ae
Pancake <nik.moskovenko@yandex.ru>
2024-09-25 19:38:38 +0300

    Добавить функционал списка друзей

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(of = "email")
@@ -13,4 +16,16 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Long> friends = new HashSet<>();
+
+    public void addFriend(Long id) {
+        friends.add(id);
+    }
+
+    public void removeFriend(Long id) {
+        if (!friends.contains(id)) {
+            throw new NotFoundException("Пользователь с id " + id + " не является другом.");
+        }
+        friends.remove(id);
+    }
 }
