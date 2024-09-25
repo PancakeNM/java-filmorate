94f2bd4101a5f325f1b5aa2a4244a911a76637e4
Pancake <nik.moskovenko@yandex.ru>
2024-09-25 19:49:22 +0300

    Добавить общее родительское исключение для исключений валидации

--- src/main/java/ru/yandex/practicum/filmorate/exception/DuplicateDataException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DuplicateDataException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class DuplicateDataException extends RuntimeException {
+public class DuplicateDataException extends ValidationException {
     public DuplicateDataException(String message) {
         super(message);
     }
